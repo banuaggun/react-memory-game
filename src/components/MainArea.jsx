690f37ed50d379bf79d81a6be2124f5e23506e7a
banuaggun <banuaggun@outlook.com>
2022-10-16 00:24:29 +0300

    Curly Brackets Bug Fixed

--- src/components/MainArea.jsx
+++ src/components/MainArea.jsx
@@ -1,23 +1,27 @@
 import { useState, useEffect } from "react";
+
 import Card from "./Card";
 
 function MainArea(props) {
     const { difficulty } = props;
-    //console.log(difficulty);
 
     const [cardArray, setCardArray] = useState([]);
+    
     const [revealedCardsArray, setRevealedCardsArray] = useState([]);
     const [selection1, setSelection1] = useState(null);
     const [selection2, setSelection2] = useState(null);
-
-    //const [revealedCardsArray, setRevealedCardsArray] = useState([]);
+    const [totalFlips, setTotalFlips] = useState(0);
+    const [flipsRemaining, setFlipsRemaining] = useState(
+        (props.difficulty * props.difficulty) / 2
+    );
+    const [canSelect, setCanSelect] = useState(true);
 
     useEffect(() => {
         let tempCardArray = [];
         for (let i = 0; i < difficulty * difficulty; i++) {
             tempCardArray.push(i);
         }
-        console.log(tempCardArray);
+        //console.log(tempCardArray);
         let shuffledArray = [];
         let revealedCards = [];
         for (let i = 0; i < difficulty * difficulty; i++) {
@@ -28,50 +32,58 @@ function MainArea(props) {
             tempCardArray.splice(randomIndex, 1);
             revealedCards.push(false);
         }
-        console.log(shuffledArray);
+        //console.log(shuffledArray);
         setCardArray(shuffledArray);
         setRevealedCardsArray(revealedCards);
-        console.log(revealedCards);
+        //console.log(revealedCards);
         // eslint-disable-next-line
     }, []);
 
     const handleSelection = (value) => {
-        console.log("clicked", value);
-        if (selection1 === null) {
-            setSelection1(value);
-            let visibleCardsArray = revealedCardsArray;
-            visibleCardsArray[value] = true;
-            setRevealedCardsArray(visibleCardsArray);
-        } else {
-            setSelection2(value);
-            let visibleCardsArray = revealedCardsArray;
-            visibleCardsArray[value] = true;
-            setRevealedCardsArray(visibleCardsArray);
+        if (canSelect) {
+            console.log("clicked", value);
+            if (selection1 === null) {
+                setSelection1(value);
+                let faceCardsArray = revealedCardsArray;
+                faceCardsArray[value] = true;
+                setRevealedCardsArray(faceCardsArray);
+            } else if (selection1 !== value) {
+                setSelection2(value);
+                let faceCardsArray = revealedCardsArray;
+                faceCardsArray[value] = true;
+                setRevealedCardsArray(faceCardsArray);
+            }
         }
     };
 
     const hideCards = () => {
-        let visibleCardsArray = revealedCardsArray;
-        visibleCardsArray[selection1] = false;
-        visibleCardsArray[selection2] = false;
-        setRevealedCardsArray(visibleCardsArray);
+        let faceCardsArray = revealedCardsArray;
+        faceCardsArray[selection1] = false;
+        faceCardsArray[selection2] = false;
+        setRevealedCardsArray(faceCardsArray);
     };
+
     const handleReset = () => {
         setSelection1(null);
         setSelection2(null);
     };
+
     useEffect(() => {
         if (selection1 !== null && selection2 !== null) {
+            setTotalFlips((prevState) => prevState + 1);
             if (selection1 % 8 === selection2 % 8) {
                 console.log("same");
                 handleReset();
+                setFlipsRemaining((prevState) => prevState - 1);
+            }else {
+                console.log("not same");
+                setCanSelect(false);
+                setTimeout(() => {
+                    hideCards();
+                    handleReset();
+                    setCanSelect(true);
+                }, 1000);
             }
-        } else {
-            console.log("not same");
-            setTimeout(() => {
-                hideCards();
-                handleReset();
-            }, 1000);
         }
     }, [selection2]);
 
@@ -79,6 +91,8 @@ function MainArea(props) {
         <div className="container my-3">
             <h3>Selection1 : {selection1}</h3>
             <h3>Selection2 : {selection2}</h3>
+            <h3>Total Flips: {totalFlips}</h3>
+            <h3>Flips Remaining: {flipsRemaining}</h3>
             <div className="row">
                 {cardArray.map((element) => {
                     return (
